9eb551ec9e61817ccb28ebc790ac6f93e1c4d2e7
zxlg <2271721552@qq.com>
2019-05-20 23:54:46 +0800

    initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+
+// allow custom header and CORS
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200) // 让options请求快速返回
+  } else {
+    next()
+  }
+})
+app.post('/hello', function (req, res) {
+  const { name } = req.body
+  res.json({
+    resMsg: `hello ${name}!`
+  })
+})
+
+app.listen(8081, function () {
+  console.log('App listening on port 8081!')
+})
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1 @click="checkCors">{{ msg }}</h1>
+    <h1>{{ resMsg }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'click here to test cors',
+      resMsg: ''
+    }
+  },
+  methods: {
+    checkCors () {
+      axios({
+        baseURL: 'http://127.0.0.1:8081',
+        url: '/hello',
+        method: 'post',
+        data: {
+          name: 'zxlg'
+        }
+      })
+        .then((res) => {
+        // handle success
+          const { resMsg } = res.data
+          this.resMsg = resMsg
+        })
+        .catch((error) => {
+        // handle error
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
